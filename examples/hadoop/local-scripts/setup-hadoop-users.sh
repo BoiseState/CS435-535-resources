f35dc5b719d17cffc2f8d7b0cffcff62d42809a5
amit <ajain@boisestate.edu>
2026-09-20 18:48:55 -0600

    update local scripts on main

---
+++ examples/hadoop/local-scripts/setup-hadoop-users.sh
@@ -0,0 +1,15 @@
+
+cd /home
+hdfs dfs -mkdir /user
+
+for name in * 
+do 
+        userName=$(echo $name | tr '[:upper:]' '[:lower:]';)
+        echo $userName
+        hdfs dfs -mkdir /user/$userName
+        hdfs dfs -chown $userName /user/$userName
+        hdfs dfs -chmod 700 /user/$userName
+        hdfs dfs -ls -d /user/$userName
+done
+
+
